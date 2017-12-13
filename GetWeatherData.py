fdb105495698e2a84c30d6785ad54ab7d92905fc
liuguangwei <kevinliu830829@163.com>
2017-12-13 13:38:30 +0800

    new

--- GetWeatherData.py
+++ GetWeatherData.py
@@ -6,6 +6,13 @@ import requests
 from datetime import datetime
 from bs4 import BeautifulSoup
 
+
+import os
+def TextToAdieo(text):
+    url = u'http://tts.baidu.com/text2audio?idx=1&tex={0}&cuid=baidu_speech_' \
+          u'demo&cod=2&lan=zh&ctp=1&pdt=1&spd=4&per=4&vol=5&pit=5'.format(text)
+    os.system('mplayer "%s"' % url)
+
 def Reptile():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit'
@@ -19,12 +26,12 @@ def Reptile():
     temp = soup.find('div', attrs={'class': 'wea_weather clearfix'}).em.getText()
     weather = soup.find('div', attrs={'class': 'wea_weather clearfix'}).b.getText()
     sd = soup.find('div', attrs={'class': 'wea_about clearfix'}).span.getText()
-    sd_num = re.search(r'\d+', sd).group()
-    sd = sd.replace(sd_num, sd_num_zh)
+    #sd_num = re.search(r'\d+', sd).group()
+    #sd = sd.replace(sd_num, sd_num_zh)
     wind = soup.find('div', attrs={'class': 'wea_about clearfix'}).em.getText()
     aqi = soup.find('div', attrs={'class': 'wea_alert clearfix'}).em.getText()
-    aqi_num = re.search(r'\d+', aqi).group()
-    aqi = aqi.replace(aqi_num, aqi_num_zh)
+    #aqi_num = re.search(r'\d+', aqi).group()
+    #aqi = aqi.replace(aqi_num, aqi_num_zh)
     info = soup.find('div', attrs={'class': 'wea_tips clearfix'}).em.getText()
     sd = sd.replace(' ', '百分之').replace('%', '')
     aqi = 'aqi' + aqi
@@ -36,5 +43,6 @@ def Reptile():
 
 if __name__=="__main__":
     data=Reptile()
+    TextToAdieo(data)
     print(data)
     pass
